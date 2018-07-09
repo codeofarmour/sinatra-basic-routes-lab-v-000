b8ed01263c913c064937491e571dd835df456869
Terri Riggle <codeofarmour@outlook.com>
2018-07-09 16:48:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is__"
+  end
+  
+  get '/hometown' do
+    "My hometown is__"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is__"
+  end
 end
